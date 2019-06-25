5f21a84db6d376e73999674b48efad643fdd3996
zhaixiaofan <842154133@qq.com>
2019-06-25 22:28:04 +0800

    test

--- app/src/main/java/com/study/zhai/playandroid/presenter/ArticleListPre.java
+++ app/src/main/java/com/study/zhai/playandroid/presenter/ArticleListPre.java
@@ -4,26 +4,26 @@ import com.google.gson.JsonObject;
 import com.study.zhai.playandroid.api.ApiService;
 import com.study.zhai.playandroid.api.ApiStore;
 import com.study.zhai.playandroid.contract.HomeListContract;
+import com.study.zhai.playandroid.log.LogUtils;
 
 import io.reactivex.Observer;
-import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.schedulers.Schedulers;
 
 public class ArticleListPre extends CommonPresenter<HomeListContract.View> implements HomeListContract.Presenter{
 
+    private int pager;
+    private static final String TAG = "ArticleListPre";
+
     @Override
     public void getArticleList(int num) {
+
         ApiStore.createApi(ApiService.class)
                 .getArticleList(1)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Observer<JsonObject>() {
                     @Override
                     public void onSubscribe(Disposable d) {
-
+                        LogUtils.d("onSubscribe");
                     }
-
                     @Override
                     public void onNext(JsonObject jsonObject) {
                         String result = jsonObject.toString();
@@ -31,14 +31,11 @@ public class ArticleListPre extends CommonPresenter<HomeListContract.View> imple
                             mView.getDemoResultOK(result);
                         }
                     }
-
                     @Override
                     public void onError(Throwable e) {
-                        if (isAttachView()) {
-                            mView.getDemoResultOK(e.toString());
-                        }
+                        LogUtils.e("error = " + e.getMessage());
+                        mView.getDemoResultOK(e.toString());
                     }
-
                     @Override
                     public void onComplete() {
 
